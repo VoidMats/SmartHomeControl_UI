335d7595b6e52e8e3d8105b26f86f8dd767fdf09
mats_home <mats.eriksson@exsensia.se>
2020-08-01 20:56:05 +0200

    Add navbar from a tutorial

--- temp_app/src/App.js
+++ temp_app/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+import Sidebar from './components/Sidebar';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Nomatch } from './pages/Nomatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+
+        <Sidebar />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={Nomatch} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
